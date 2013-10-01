d2001485cbddad820cb63ef182ce014274e2c5dc
Elad Meidar <elad@eizesus.com>
2013-10-01 20:53:48 +0300

    [fix]

--- lib/site_classifier/extractor.rb
+++ lib/site_classifier/extractor.rb
@@ -12,6 +12,12 @@ module SiteClassifier
       @lang = lang.downcase
     end
 
+    def self.debug(string)
+      if SiteClassifier.configuration.debug?
+        puts "#{Time.now.to_i} - #{string}"
+      end
+    end
+
     # Normalize site language
     def validate_lang
       if EasyTranslate::LANGUAGES.keys.include?(@lang)
@@ -24,17 +30,33 @@ module SiteClassifier
     # Extract most significant tags
     def most_significant
       most_sig = []
-      if !description.nil?
-        if tags.any?
-          most_sig = tags.select {|tag| self.description.downcase.include?(tag)}.collect {|tag| tag.singularize }
-        else
-          most_sig = word_frequency.keys.select {|tag| self.description.downcase.include?(tag)}.collect {|tag| tag.singularize }
-        end
+      # if !description.nil?
+      #   if tags.any?
+      #     most_sig = tags.select {|tag| self.description.downcase.include?(tag)}.collect {|tag| tag.singularize }
+      #   else
+      #     most_sig = word_frequency.keys.select {|tag| self.description.downcase.include?(tag)}.collect {|tag| tag.singularize }
+      #   end
+      # end
+
+      description.to_s.split.each do |word|
+        self.word_frequency[word] ||= 0
+        self.word_frequency[word] += 1
       end
 
       if most_sig.empty?
-        most_sig = self.word_frequency.keys
+        most_sig = self.word_frequency.reject {|k,v| v < 3}.keys
+        most_sig.flatten!
       end
+
+      if description && tags.any?
+        tags.each do |tag|
+          if description.include?(tag)
+            most_sig << tag.singularize
+          end
+        end
+      end
+      
+      most_sig.uniq!
 
       self.validate_lang
 
@@ -65,6 +87,7 @@ module SiteClassifier
     def self.parse_site(url = "")
       return if url == "" || url.nil?
 
+      debug("getting #{url}")
       html = Nokogiri::HTML(self.get(url).parsed_response)
 
       tags = []
@@ -74,30 +97,71 @@ module SiteClassifier
 
       begin
         page_lang = html.search("html").first["lang"].to_s.slice(0..1)
+        debug("found lang in html tag - #{page_lang}")
       rescue
       end
 
       begin
         page_lang = html.search("html").first["xml:lang"].to_s.slice(0..1)
+        debug("found lang in html tag (xml:lang) - #{page_lang}")
       rescue
       end
 
       begin
         tags = html.search('meta[name="keywords"]').first["content"].split(",").collect(&:strip).collect(&:downcase)
-        description = html.search('meta[name="description"]').first["content"]
+        debug("Tags - #{tags.inspect}")
       rescue
+        debug("no tags found")
       end
 
       if tags.empty?
+        begin
+          tags = html.search('meta[property="keywords"]').first["content"].split(",").collect(&:strip).collect(&:downcase)
+          debug("Tags - #{tags.inspect}")
+        rescue
+          debug("no tags found")
+        end
+      end
+      begin
+        description = html.search('meta[name="description"]').first["content"]
+        debug("Decription meta found")
+      rescue
+      end
+
+      if description.nil?
+        begin
+          description = html.search('meta[property="og:description"]').first["content"]
+          debug("Facebook og:description found")
+        rescue
+        end
+      end
+
+      if description.nil?
+        begin
+          description = html.search('meta[name="og:description"]').first["content"]
+          debug("Facebook og:description found")
+        rescue
+        end
+      end
+
+      if tags.empty?
+        debug("no tags, parsing body")
         word_hash = Hash.new(0)
-        all_text = html.search("p").collect {|p| p.text.strip }.collect {|text| text.split.collect(&:strip)}.flatten.reject {|word| word.size < 4}
+        all_text = []
+        # all_text = html.search("p").collect {|p| p.text.strip }.collect {|text| text.split.collect(&:strip)}.flatten.reject {|word| word.size < 4}
+        # debug("p's extracts - #{all_text.inspect}")
         if all_text.empty?
           all_text = html.search("div").collect {|p| p.text.strip }.collect {|text| text.split.collect(&:strip)}.flatten.reject {|word| word.size < 4}
+          debug("divs extracts - #{all_text.inspect}")
         end
-        all_text.each do |word|
+        all_text += description.to_s.split
+
+        all_text.flatten.each do |word|
           word_hash[word] += 1
         end
-        word_hash.reject! {|k,v| v < 2 }
+        debug("final word hash - #{word_hash.inspect}")
+        word_hash.reject! {|k,v| v < 3 || k.size == 1 || k.include?(".") || k.include?("'") || k.include?("(") || k.include?(":") || k.include?("]")}
+        
       end
       self.new(url, tags, word_hash, description, page_lang)
     end
